dd1186479a5641f938a4be23fe79cebf8c5ce64e
Nightmare2202 <ashutoshinde23@gmail.com>
2021-09-20 10:01:39 +0530

    fixing heroku

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const server = express();
 const AuthRouter = require('./routes/auth')
 const NewsRouter = require('./routes/news')
 
-server.use(cors({origin : 'https://news-app-22.herokuapp.com'}));
+server.use(cors({origin : '*'}));
 server.use(bodyParser.urlencoded({extended : false}));
 server.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 server.use('/auth', AuthRouter);
 server.use('/news', NewsRouter);
